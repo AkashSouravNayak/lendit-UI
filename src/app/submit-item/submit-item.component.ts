f20863c11e1ae546ae0b14599fdf5df595c4cdc0
golukumar21 <golukumar21@gmail.com>
2019-03-09 19:30:17 +0530

    image upload feature

--- src/app/submit-item/submit-item.component.ts
+++ src/app/submit-item/submit-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 
 
 @Component({
@@ -7,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submit-item.component.less']
 })
 export class SubmitItemComponent implements OnInit {
-
+  @ViewChild('fileInput') fileInput: ElementRef;
+  @ViewChild('preview') preview: ElementRef;
+  showImg: boolean = false;
+  hidePlus: boolean = true;
+  
   constructor() { }
 
   ngOnInit() {
   }
+  uploadImage () {
+    debugger;
+    this.showImg = true;
+    let el: HTMLElement = this.fileInput.nativeElement as HTMLElement;
+    el.click();
+  }
+  previewFile(e) {
+    debugger;
+    var reader;
+    
+    let imgFile: HTMLElement = this.preview.nativeElement as HTMLElement;
+    if (e.currentTarget.files && e.currentTarget.files[0]) {
+      reader = new FileReader();
 
+      reader.onload = function (e) {
+        debugger;
+        imgFile.setAttribute('src', e.target.result);;
+      }
+
+      reader.readAsDataURL(e.currentTarget.files[0]);
+      this.hidePlus = false;
+    }
+  }
 }
